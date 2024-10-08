fc95ba24226f7daea0326ce71595b1f22797bb5d
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2024-10-08 12:03:56 +0900

    update, add: Restructured dirs to make "AI討論" the landing page #20

--- public/client.js
+++ public/client.js
@@ -1,36 +1,72 @@
 // Get HTML element IDs
 const ids = {
-    askButton: "askButton",
+    debateButton: "debateButton",
     inputText: "inputText",
     outputText: "outputText",
+    characterID: "character",
+    organizeButton: "organizeButton",
+    organizedText: "organizedText",
     useLocalApi: "useLocalApi",
     useLocalApiText: "useLocalApiText",
     loadingText: "loading",
-    dotsText: "dots"
-}
+    dotsText: "dots",
+    debateFinishButton: "debateFinish",
+    finishingText: "finishingText",
+    finishing: "finishing",
+    summary: "summary"
+};
 const elements = {};
 Object.keys(ids).forEach(key => elements[key] = document.getElementById(ids[key]));
 
-const badContentNotice = "不適切なコンテンツを含む回答が生成されてしまいました。質問を変えて再度お試しください。";
+const organizeButtonText = elements.organizeButton.textContent;
+const badContentNotice = "不適切なコンテンツを含む回答が生成されてしまいました。論題を変えて再度お試しください。";
+
+let orderInt = 0;
+let isFinish = false;
+
+let geminiText = null;
+let cohereText = null;
 
 // テキスト入力
 elements.inputText.addEventListener("input", () => {
-    const isEmpty = inputText.value.trim() === "";
-    elements.askButton.disabled = isEmpty;
+    const isEmpty = elements.inputText.value.trim() === "";
+    elements.organizeButton.disabled = isEmpty;
 });
 
 // テキストエリアで Ctrl+Enter
 elements.inputText.addEventListener("keydown", (event) => {
     if (event.ctrlKey && event.key === "Enter") {
         elements.outputText.innerHTML = "";
-        askButtonClicked(inputText.value);
+        elements.summary.innerHTML = "";
+        elements.debateButton.disabled = false;
+        organizeButtonClicked(elements.inputText.value);
     }
 });
 
-// 質問する! ボタン押下
-elements.askButton.addEventListener("click", () => {
+// 論点を整理する ボタン押下
+elements.organizeButton.addEventListener("click", () => {
     elements.outputText.innerHTML = "";
-    askButtonClicked(inputText.value);
+    elements.summary.innerHTML = "";
+    elements.debateButton.disabled = false;
+    organizeButtonClicked(elements.inputText.value);
+});
+
+// 討論開始! ボタン押下
+elements.debateButton.addEventListener("click", () => {
+    
+    // ボタン制御
+    elements.organizeButton.disabled = true;
+    elements.debateButton.disabled = true;
+    elements.debateFinishButton.disabled = false;
+
+    debateButtonClicked();
+});
+
+// 終了 ボタン押下
+elements.debateFinishButton.addEventListener("click", () => {
+    isFinish = true;
+    elements.finishingText.innerText = "今のターンで終了します";
+    elements.finishing.style.display = "inline-block";
 });
 
 if(window.location.hostname !== "localhost"){
@@ -38,24 +74,83 @@ if(window.location.hostname !== "localhost"){
     elements.useLocalApiText.innerHTML = `<span title='ローカル環境でのみ使用できます'>${useLocalApiText.innerHTML}</span>`;
 } 
 
-async function askButtonClicked(input) {
+async function organizeButtonClicked(input) {
+
+    // Geminiに論点整理をさせ表示する
+    const geminiOrganize = await fetch("api/gemini", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ order: orderInt, text: input }),
+    });
+    const geminiOrganizeRes = await geminiOrganize.text();
+    if (geminiOrganizeRes.includes("A server error has occurred\n\nFUNCTION_INVOCATION_FAILED\n")) {
+        elements.organizedText.innerHTML = badContentNotice;
+    } else elements.organizedText.innerHTML = geminiOrganizeRes;
+
+    elements.organizeButton.textContent = "再生成する";
+
+    // ボタン有効化
+    elements.debateButton.disabled = false;
+}
+
+async function debateButtonClicked() {
+    
     try{
+        // テキストを戻す
+        elements.organizeButton.textContent = organizeButtonText;
+
         // ローディング表示
         toggleLoading(true, "考え中");
+
+        // 交互に異なるLLMを使用
+        orderInt++;
         
-        // 回答文生成
-        const gemini = await fetchAndParse("/api/gemini", -1, input, "application/json");   
-        if (gemini.includes("A server error has occurred\n\nFUNCTION_INVOCATION_FAILED\n")){
-            elements.outputText.innerHTML = badContentNotice;
-        } else elements.outputText.innerText = gemini;
+        if (orderInt % 2 !== 0) {
+
+            // Geminiで賛成意見生成  表示  議題と主張はserver.jsの変数に既に存在  
+            geminiText = await fetchAndParse("api/gemini", orderInt, "application/json", "json");
+            showOutput("geminiDebate", orderInt, geminiText.assertion);
+        } else {
+
+            // Cohereで反対意見生成
+            cohereText = await fetchAndParse("api/cohere", orderInt, "text/plain", "text");
+            showOutput("cohereDebate", orderInt, cohereText);
+        }
         
         // ローディング表示変更
         toggleLoading(true, "発声準備中");
         
         // 音声処理
-        playVoice(gemini);
+        playVoice(orderInt % 2 !== 0 ? geminiText.assertion : cohereText);
+
+        // 計10回の意見生成(各5回)で終了
+        if (orderInt >= 10) isFinish = true;
     } catch (error){
-        console.error("エラー: ", error);
+        console.error(`エラー: ${error}`);
+    }
+}
+
+// LLMエンドポイント通信用関数
+async function fetchAndParse(url, order, type, returnType) {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": type,
+        },
+        body: JSON.stringify({ order: order, text: "" }),
+    });
+    return returnType === "json" ? response.json() : response.text();
+}
+
+// 回答表示用関数
+function showOutput(className, order, text) {
+    elements.outputText.innerHTML += `<br><div class="${className}">${text}</div>`;
+    
+    // 2週目以降ならば要約も表示
+    if (order !== 1 && className === "geminiDebate") {
+        elements.summary.innerHTML = geminiText.summary;
     }
 }
 
@@ -66,28 +161,16 @@ function toggleLoading(isLoading, text) {
     elements.loadingText.innerText = text;
 }
 
-// LLMエンドポイント通信用関数
-async function fetchAndParse(url, order, text, type) {
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": type,
-        },
-        body: JSON.stringify({ order: order, text: text }),
-    });
-    return response.text();
-}
-
 // 音声再生準備用関数
 async function playVoice(text) {
-    const speakerID = "3";
+    const speakerID = orderInt % 2 !== 0 ? "3" : elements.characterID.value;
     
     // ローカル環境なら高速版またはローカル版を利用し、それ以外 (Vercel)ならストリーミング版を利用する
     if (window.location.hostname === "localhost") {
-        elements.useLocalApi.checked ? await synthesizeAudioLocally("/api/voicevox/local", text, speakerID)
-            : await synthesizeAudioLocally("/api/voicevox/fast", text, speakerID);
+        elements.useLocalApi.checked ? await synthesizeAudioLocally("api/voicevox/local", text, speakerID)
+            : await synthesizeAudioLocally("api/voicevox/fast", text, speakerID);
     }
-    else await synthesizeAudioStreaming("/api/voicevox/streaming", text, speakerID);
+    else await synthesizeAudioStreaming("api/voicevox/streaming", text, speakerID);
 }
 
 // ローカル環境での合成用関数
@@ -99,7 +182,7 @@ async function synthesizeAudioLocally(url, text, speaker) {
     });
     if (!response.ok) {
         // 合成失敗時はストリーミング版を使う
-        synthesizeAudioStreaming("/api/voicevox/streaming", text, speaker);
+        synthesizeAudioStreaming("api/voicevox/streaming", text, speaker);
         return;
     }    
     const audioBlob = await response.blob();
@@ -125,5 +208,25 @@ async function synthesizeAudioStreaming(url, text, speaker) {
 async function handleAudio(audio, audioUrl) {
     audio.play();
     audio.addEventListener("playing", () => toggleLoading(false, ""));
+    audio.addEventListener("ended", () => debateEnded());
     if (audioUrl) audio.onended = () => URL.revokeObjectURL(audioUrl);
+}
+
+// 討論継続判断用関数
+function debateEnded() {
+    if (isFinish) resetDebate();
+    else debateButtonClicked();
+}
+
+// 初期化用関数
+function resetDebate() {
+    isFinish = false;
+    orderInt = 0;
+
+    elements.finishingText.innerText = "";
+    elements.finishing.style.display = "none";
+    toggleLoading(false, "");
+
+    elements.organizeButton.disabled = false;
+    elements.debateFinishButton.disabled = true;
 }
