b4a95f3caf1abfaeb6654b57d3a2afe1c1381d0c
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2025-03-15 12:31:57 +0900

    update: Geminiのモデル変更

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ function formatResponseText(responseText, toHTML) {
 
 // Gemini用初期化関数
 function initializeChatSession() {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-001" });
     
     return model.startChat({
         generationConfig: {
